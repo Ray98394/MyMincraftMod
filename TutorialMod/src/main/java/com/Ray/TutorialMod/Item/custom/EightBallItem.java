94964fb3090bbf53e6b4d56fed259af59b86f5d5
Ray98394 <3408555350@qq.com>
2024-06-03 17:01:36 +0800

    2024/6/3

--- TutorialMod/src/main/java/com/Ray/TutorialMod/Item/custom/EightBallItem.java
+++ TutorialMod/src/main/java/com/Ray/TutorialMod/Item/custom/EightBallItem.java
@@ -1,13 +1,18 @@
 package com.Ray.TutorialMod.Item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Random;
 
 public class EightBallItem extends Item {
@@ -22,6 +27,16 @@ public class EightBallItem extends Item {
             player.getCooldowns().addCooldown(this,20);
         }
         return super.use(level, player, interactionHand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()){
+            components.add(Component.literal("这是一个可以通过右键来获得随机数的物品").withStyle(ChatFormatting.ITALIC));
+        }else {
+            components.add(Component.literal("按下shift获得更多信息").withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(stack, level, components, flag);
     }
 
     private void outputRandomNumber(Player player){
--- TutorialMod/src/main/java/com/Ray/TutorialMod/block/ModBlocks.java
+++ TutorialMod/src/main/java/com/Ray/TutorialMod/block/ModBlocks.java
@@ -4,12 +4,15 @@ package com.Ray.TutorialMod.block;
 import com.Ray.TutorialMod.Item.ModCreativeModTab;
 import com.Ray.TutorialMod.Item.ModItem;
 import com.Ray.TutorialMod.TutorialMod;
+import com.Ray.TutorialMod.block.custom.BlueBerryCropBlock;
 import com.Ray.TutorialMod.block.custom.JumpyBlock;
+import com.Ray.TutorialMod.block.custom.ZirconLampBlock;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -38,6 +41,14 @@ public class ModBlocks {
     public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
             ()->new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)),ModCreativeModTab.TUTORIAL_TAB);
 
+    public static final RegistryObject<Block> ZIRCON_LAMP = registerBlock("zircon_lamp",
+            ()-> new ZirconLampBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(ZirconLampBlock.LIT) ? 15:0)),ModCreativeModTab.TUTORIAL_TAB);
+
+    public  static final RegistryObject<Block> BLUEBERRY_CROP = BLOCKS.register("blueberry_crop",
+            () -> new BlueBerryCropBlock(BlockBehaviour.Properties.copy(Blocks.WHEAT)));
+
+    //注册函数
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name,block);
         registryBlockItem(name,toReturn,tab);
